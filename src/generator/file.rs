fa81eef1f4cf95e74d59dbbcfaf60c8f72478e89
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-06-04 13:12:29 +0200

    [FIX] write_file()
    Made it so it appends inside a file that already exists

--- src/generator/file.rs
+++ src/generator/file.rs
@@ -1,11 +1,14 @@
-use std::{fs::File, io::Write};
+use std::{
+    fs::{File, OpenOptions},
+    io::Write,
+};
 
 pub fn create_file(path: &str) -> Result<File, std::io::Error> {
     File::create(path)
 }
 
 pub fn write_file(path: &str, text: &str) -> Result<(), std::io::Error> {
-    match File::open(path) {
+    match OpenOptions::new().append(true).open(path) {
         Ok(mut file) => file.write_all(text.as_bytes()),
         Err(e) => Err(e),
     }
